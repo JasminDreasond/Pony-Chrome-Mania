627537efb76e350442b0f264e2ce171f5ebeb090
Marcelo Seidel Jr <jackiedevpe@outlook.com.br>
2017-04-09 06:12:37 -0300

    Delete errorconfig.js

--- src/src/js/errorconfig.js
+++
@@ -1,9 +0,0 @@
-function errorpker(errormsg){
-alert(errormsg);
-}
-
-function dateupdatelocalset(){chrome.storage.local.get(function(dateset){
-$("#refreshdate").text(dateset.daterefresh);
-})}
-
-var specialpagesource = false
